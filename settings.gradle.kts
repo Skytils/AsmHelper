2d6dd0465e326fc37db0f498fba220cd43eb853c
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-09-08 00:23:00 -0400

    Disable all subprojects except for 1.8.9

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,9 +21,9 @@ pluginManagement {
 rootProject.buildFileName = "root.gradle.kts"
 
 listOf(
-    "1.8.9",
-    "1.12.2",
-    "1.15.2",
+    "1.8.9"
+//    "1.12.2",
+//    "1.15.2"
 //    "1.16.2"
 //    "1.16.2-fabric"
 ).forEach { version ->
